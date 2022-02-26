b4278ba9ec199bcdd41811a0fbd1fd2f1d6032aa
Isaac Kimura <isaac.kimura@iprospect.com>
2022-02-26 05:33:51 +0300

    Done designing the floating navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./navbar.css";
+import { FaHome } from "react-icons/fa";
+import { SiAboutdotme } from "react-icons/si";
+import { SiBookstack } from "react-icons/si";
+import { RiServiceFill } from "react-icons/ri";
+import { TiMessages } from "react-icons/ti";
+
+import { useState } from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href='#'
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <FaHome />
+      </a>
+      <a
+        href='#about'
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <SiAboutdotme />
+      </a>
+      <a
+        href='#experience'
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <SiBookstack />
+      </a>
+      <a
+        href='#services'
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceFill />
+      </a>
+      <a
+        href='#contact'
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <TiMessages />
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
