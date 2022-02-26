5f693e575309c2146a09aa6f7407052008a75545
Isaac Kimura <isaac.kimura@iprospect.com>
2022-02-26 05:29:09 +0300

    Added ids to respective sections to be used in tha nav for navagation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import HeaderSocial from "./HeaderSocial";
+import ME from "../../assets/dev-5.jpeg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm </h5>
+        <h1>Isaac K. Kimura</h1>
+        <h5 className='text-light'>Full-stack Develloper</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className='me'>
+          <img src={ME} alt='Isaac Karanja Kimura' />
+        </div>
+        <a href='#contact' className='scroll__down'>
+          scroll down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
