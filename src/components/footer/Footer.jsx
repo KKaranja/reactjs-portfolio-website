329552d260e93673f8f207e55c3a88e015d34896
Isaac Kimura <isaac.kimura@iprospect.com>
2022-02-27 22:31:16 +0300

    Done designing the footer and the whole website!

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./footer.css";
 
+import { BsTwitter, BsLinkedin, BsFacebook, BsInstagram } from "react-icons/bs";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href='#' className='footer__logo'>
+        KIMURA
+      </a>
+      <ul className='permalinks'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#abou'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a>
+        </li>
+        <li>
+          <a href='#services'>Services</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#testimonial'>Testimonial</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+      <div className='footer__social'>
+        <a href='https://facebooc.com'>
+          <BsFacebook className='footer__social-icon' />
+        </a>
+        <a href='https://twiter.com'>
+          <BsTwitter className='footer__social-icon' />
+        </a>
+        <a href='https://linkedin.com'>
+          <BsLinkedin className='footer__social-icon' />
+        </a>
+        <a href='https://intagram.com'>
+          <BsInstagram className='footer__social-icon' />
+        </a>
+      </div>
+      <div className='footer__copyright'>
+        <small>&copy; Isaac Kimura</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
